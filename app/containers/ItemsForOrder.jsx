ec004cbd7a7d568a65dc4e64069b7869cae0511f
Deepan <deepan.agarwal@hcl.com>
2018-12-24 19:30:32 +0530

    Review comments fixed

--- app/containers/ItemsForOrder.jsx
+++ app/containers/ItemsForOrder.jsx
@@ -1,14 +1,21 @@
+// import dependency files
 import React from 'react';
 import { connect } from 'react-redux';
 import _ from 'lodash';
 import { AgGridReact } from 'ag-grid-react';
+import { itemsPriceWeight, itemsCourierCharges } from 'app_assets/mockData/itemsData.js'
+
+// import CSS files
 import 'ag-grid-community/dist/styles/ag-grid.css';
 import 'ag-grid-community/dist/styles/ag-theme-blue.css'
-import { itemsPriceWeight, itemsCourierCharges } from 'app_assets/mockData/itemsData.js'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'app_assets/css/itemsForOrder.css'
+
 class ItemsForOrder extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      displayPackageHeadingFlag: false,
       finalPackage: [],
       packageFlag: false,
       columnDefs: [
@@ -37,7 +44,7 @@ class ItemsForOrder extends React.Component {
     }
   }
   /**
-* @desc shows error message on click of Cancel button
+* @desc react life-cycle method
 * @param {null}  
 * @returns  {void} 
 */
@@ -46,6 +53,11 @@ class ItemsForOrder extends React.Component {
       rowData: itemsPriceWeight
     })
   }
+  /**
+* @desc gets called when place order button is clicked
+* @param {null}  
+* @returns  {void} 
+*/
   onButtonClick = () => {
     let totalPrice = 0;
     let totalWeight = 0;
@@ -93,27 +105,21 @@ class ItemsForOrder extends React.Component {
     averagePrice = Math.ceil(totalPrice / numberOfPackages);
     let package2 = this.packageDetails(numberOfPackages, selectedData, equalWeights, remainingPrice, remainingWeight, averagePrice);
     this.setState({
-      finalPackage: package2
-    },function(){
-      for (let a = 0; a < this.state.finalPackage.length; a++) {
-      let packageName = "package_" + a
-      let items = "";
-      let packageTotalPrice = 0;
-      let packageTotalWeight = 0;
-      for (let b = 0; b < this.state.finalPackage[a].length; b++) {
-        items += this.state.finalPackage[a][b].Name + " "
-        packageTotalPrice += parseInt(this.state.finalPackage[a][b].Price)
-        packageTotalWeight += parseInt(this.state.finalPackage[a][b].Weight)
-      }
-      alert(
-        packageName + "contains" + " : "
-        +"items :" + items
-        + "Total Price :" + packageTotalPrice 
-        + "Total Weight :" + packageTotalWeight
-        )
-    }
+      finalPackage: package2,
+      displayPackageHeadingFlag: true
+    }, function () {
     })
   }
+  /**
+* @desc calls internally by button click function and processed the input params data and return an array with all the packages
+* @param {number} numberOfPackages
+* @param {array} selectedData
+* @param {number} equalWeights
+* @param {number} remainingPrice
+* @param {number} remainingWeight
+* @param {number} averagePrice
+* @returns {array} finalPackage
+*/
   packageDetails(numberOfPackages, selectedData, equalWeights, remainingPrice, remainingWeight, averagePrice) {
     let package2 = [];
     let finalPackage = [];
@@ -165,33 +171,60 @@ class ItemsForOrder extends React.Component {
     }
     return finalPackage;
   }
-  onClickSelectAll() {
-    this.gridApi.selectAll();
-  }
   render() {
     return (
       <div>
-        <div id="myGrid" style={{ height: '500px', width: '600px' }} className="ag-theme-blue">
-          <AgGridReact
-            rowSelection="multiple"
-            columnDefs={this.state.columnDefs}
-            rowData={this.state.rowData}
-            onGridReady={params => this.gridApi = params.api}
-            >
-          </AgGridReact>
+        <div className={"heading"}>
+          <header>
+            <h2>Items List</h2>
+          </header>
         </div>
-        <button onClick={this.onButtonClick}>Place Order</button>
+        <div className={"col-md-12 form-inline"}>
+          <div id="myGrid" style={{ height: '500px', width: '300px' }} className="ag-theme-blue gridPosition col-md-2 backgrndCol">
+            <AgGridReact
+              rowSelection="multiple"
+              columnDefs={this.state.columnDefs}
+              rowData={this.state.rowData}
+              onGridReady={params => this.gridApi = params.api}
+              >
+            </AgGridReact>
+          </div>
+          <div className="col-md-4">
+            {this.state.displayPackageHeadingFlag ? <h3>Packages</h3> : null}
+            {
+              function () {
+                if (this.state.finalPackage.length > 0) {
+                  let displayPackages = "";
+                  for (let a = 0; a < this.state.finalPackage.length; a++) {
+                    let packageName = "package_" + a
+                    let items = "";
+                    let packageTotalPrice = 0;
+                    let packageTotalWeight = 0;
+                    for (let b = 0; b < this.state.finalPackage[a].length; b++) {
+                      items += this.state.finalPackage[a][b].Name + " "
+                      packageTotalPrice += parseInt(this.state.finalPackage[a][b].Price)
+                      packageTotalWeight += parseInt(this.state.finalPackage[a][b].Weight)
+                    }
+                    displayPackages += packageName + " contains" + " : "
+                      + "items :" + items
+                      + "Total Price :" + packageTotalPrice
+                      + "Total Weight :" + packageTotalWeight + "\n"
+                  }
+                  return (
+                    <span>
+                      {displayPackages}
+                    </span>
+                  )
+                }
+              }.call(this)}
+          </div>
+        </div>
+        <div className={"col-md-2 btnStyle"}>
+          <button className={"btn btn-primary float-right"} onClick={this.onButtonClick}>Place Order</button>
+        </div>
       </div>
-
     )
   }
 }
 
-
-function mapStateToProps(state) {
-  return {
-
-  }
-}
-
-export default connect(mapStateToProps)(ItemsForOrder);
+export default (ItemsForOrder);
